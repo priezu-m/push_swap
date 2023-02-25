0b808b98eed4aafe2c9ae86d63e01ff9d74583a0
Peru Riezu <priezu-m@student.42madrid.com>
2023-02-25 21:09:20 +0100

    go

--- sort_stacks.c
+++ sort_stacks.c
@@ -6,7 +6,7 @@
 /*   github:   https://github.com/priezu-m                                    */
 /*   Licence:  GPLv3                                                          */
 /*   Created:  2023/02/22 17:54:57                                            */
-/*   Updated: 2023/02/25 17:39:36 by anon             ###   ########.fr       */
+/*   Updated: 2023/02/25 19:49:48 by anon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,20 @@ static void	split_b(t_stacks *stacks, int chunk_size);
 
 static void	split_a(t_stacks *stacks, int chunk_size)
 {
+	int	a_pivot;
+	int	b_pivot;
+
+	a_pivot = ceil_div(stacks->stack_a.current_size, 2);
+	a_pivot = a_pivot + !!(a_pivot % 2);
+	b_pivot = a_pivot / 2;
+	while (stacks->stack_a.current_size > 4)
+	{
+
+		a_pivot = ceil_div(stacks->stack_a.current_size, 2);
+		a_pivot = a_pivot + !!(a_pivot % 2);
+		b_pivot = (a_pivot + (stacks->stack_a.max_size - stacks->stack_a.current_size)) / 2;
+	}
+
 }
 
 static void	split_b(t_stacks *stacks, int chunk_size)
